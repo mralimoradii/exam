4ccba825f8c0039e44e41028aa07e12577461653
mralimoradii <152193018+mralimoradii@users.noreply.github.com>
2023-12-15 17:07:04 +0330

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,44 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'test';
+
+ mac:{img3:string,paraf:string,price:string,cpu:string,ram:string,rom:string,grafic:string,resoulation:string,bime:string,color:{nameclo:string,cod:string}[],Insurance:{name:string,mony:string}[], waranti:{name:string,month:string,}[]}=
+      {
+       img3:"https://dkstatics-public.digikala.com/digikala-products/0453261f5778b624dae7dda88a414fe8fcf8d109_1671472045.jpg?x-oss-process=image/resize,m_lfit,h_800,w_800/format,webp/quality,q_90",
+       paraf:"لپ تاپ 13.6 اینچی اپل مدل MacBook Air-B M2 2022",
+       price:"۵۳۴۸۰۰۰۰",
+       cpu:"m2",
+       ram:"۸گیگابایت",
+       rom:"۲۵۶گیگابایت",
+       grafic:"اپل",
+       resoulation:"2560x1664",
+       bime:"بیمه تجهیزات بیمه پارسیان",
+       color:[
+         {
+           nameclo:"سرمه ای",
+           cod:"#000000"
+         },{
+           nameclo:"خاکستری",
+           cod:"#fbfbfb"
+         },{
+          nameclo:"نقره ای",
+          cod:""
+         }
+       ],
+       Insurance:[
+         {
+           name:"بیمه تجهیزات دیجیتال - بیمه پارسیان",
+           mony:"۱,۱۸۳,۷۴۰"
+         }
+         
+       ],
+       waranti:[{
+        name:"رایتاک",
+        month:"گارانتی ۱۸ ماهه راسا همراه سورن"
+      }
+       ]
+      }
+
+
+ 
 }
